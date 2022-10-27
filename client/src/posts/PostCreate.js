b290198ad5551ef44fc96497bfe23b96965652a2
elgun <elgun.abasquliyev2@gmail.com>
2022-10-27 04:16:21 +0400

    Blog with basic microservice architecture and kubernetes

---
+++ client/src/posts/PostCreate.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import axios from "axios";
+
+const PostCreate = () => {
+  const [title, setTitle] = useState("");
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    await axios.post(
+      "http://posts.dev.com/posts/create",
+      { title },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    setTitle("");
+  };
+
+  const inputHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={inputHandler}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary my-1">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default PostCreate;
